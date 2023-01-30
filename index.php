6e40fe259aa6560a968a698c980cdcb4dc3d4773
Rival-100 <123920234+Rival-100@users.noreply.github.com>
2023-01-30 09:59:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,646 @@
+
+<!DOCTYPE html>
+
+<head>
+	
+<meta charset="utf-8">
+	
+<meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Temple Booking</title>
+	
+	
+<!-- [ FONT-AWESOME ICON ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+
+	
+<!-- [ PLUGIN STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="shortcut icon" type="image/x-icon" href="images/505004.png">
+	
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+	
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+        
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+	
+<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+	
+<!-- [ Boot STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+       
+<!-- [ DEFAULT STYLESHEET ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="css/style.css">
+        
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	
+<link rel="stylesheet" type="text/css" href="css/color/rose.css">
+        
+
+</head>
+<body >
+
+<!-- [ LOADERs ]
+
+================================================================================================================================-->
+	
+    <div class="preloader">
+    
+<div class="loader theme_background_color">
+        
+<span></span>
+      
+    
+</div>
+</div>
+<!-- [ /PRELOADER ]
+
+=============================================================================================================================-->
+
+<!-- [WRAPPER ]
+
+=============================================================================================================================-->
+
+<div class="wrapper">
+  
+<!-- [NAV]
+ 
+============================================================================================================================-->    
+   
+<!-- Navigation
+    ==========================================-->
+    
+<nav  class=" nim-menu navbar navbar-default navbar-fixed-top">
+      
+<div class="container">
+        
+<!-- Brand and toggle get grouped for better mobile display -->
+        
+<div class="navbar-header">
+          
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            
+<span class="sr-only">Toggle navigation</span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+          
+</button>
+            
+<a class="navbar-brand" href="index.html">Temple<span class="themecolor"> KC </span>Booking</a>
+        
+</div>
+
+        
+<!-- Collect the nav links, forms, and other content for toggling -->
+        
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          
+<ul class="nav navbar-nav navbar-right">
+            
+<li><a href="#home" class="page-scroll"><h3>Home</h3></a></li>
+          
+                                   
+<li><a href="#one" class="page-scroll"><h3>Temple Description</h3></a></li>
+
+<li><a href="xml/rate.html" class="page-scroll"><h3>Services</h3></a></li>
+                                 
+<li><a href="z/ckc.html" class="page-scroll"><h3>Temples </h3></a></li>
+            
+<li><a href="#four" class="page-scroll"><h3>Contact</h3></a></li>
+
+<li><a href="http://localhost:8080/download/login.jsp" class="page-scroll"><h3>logIn</h3></a></li>
+
+
+<li><a href="admin/" class="page-scroll"><h3>Admin</h3></a></li>
+
+
+          
+</ul>
+        
+</div>
+<!-- /.navbar-collapse -->
+      
+</div><!-- /.container-fluid -->
+    
+</nav>
+
+
+   
+<!-- [/NAV]
+ 
+============================================================================================================================--> 
+    
+   
+<!-- [/MAIN-HEADING]
+ 
+============================================================================================================================--> 
+   
+<section class="main-heading" id="home">
+       
+<div class="overlay">
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements="Online Temple,Online Temple,Online Temple"></span></h1>
+ 
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements=" Ticket Booking, Ticket Booking, Ticket Booking"></span></h1>
+       
+<p class="main-heading-text">WELCOME TO,<br/>ONLINE TEMPLE TICKET BOOKING</p>
+        
+<div class="btn-bar">
+          
+<a href="reserved.php" class="btn btn-custom theme_background_color">Book Now</a>
+                 
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+      
+   
+</section>
+ 
+<section class="main-heading" id="home">
+       
+<div class="overlay">
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="main-heading-title">BE GOOD FEEL GOOD</h1>
+        
+<p class="main-heading-text">All are comes for some sort of problem or be get peace. but
+<br/>definity, after you visit you fell good you will do good. <br/> By our booking sysytem helps you to enter the temple by stress less queue or rush in crowed. </p>
+        
+<div class="btn-bar">
+          
+<a href="#" class="btn btn-custom theme_background_color">Ge Started</a>
+          
+<a href="#" class="btn btn-custom-outline">Contact Us</a>
+        
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+      
+   
+</section>
+    
+ 
+<!-- [/MAIN-HEADING]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [ABOUT US]
+ 
+============================================================================================================================-->
+ 
+<section class="aboutus white-background black" id="one">
+     
+<div class="container">
+         
+<div class="row">
+             
+<div class="col-md-12 text-center black">
+                 
+<h3 class="title">ABOUT <span class="themecolor">US</span></h3>
+            
+<p class="a-slog">Online Temple Ticket booking was created by chandru.</p> 
+<p class="a-slog">-----------------------------------------------------</p> 
+<p class="a-slog">we are connect the people who are not able to visited the temple in rush ways. our aim is all people come in the temple in feel free stress about queue and rush ways..</p> 
+
+             
+</div>
+         
+</div>
+         
+<div class="gap">
+             
+         
+</div>
+         
+         
+<div class="row about-box">
+          
+<div class="col-sm-4 text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-search"></i>
+              
+<h4>WHEN WAS TEMPLE BUILD?</h4>
+              
+<p class="black">The earliest example of this kind was a cave temple that was built in around 400 B.C. at Bhaje containing images of learned Parasurama with Indra and Surya. After this came the turn of stone temples, as brick and wood were perishable materials.</p>
+            
+</div> <!-- / margin -->
+          
+</div> <!-- /col -->
+          
+<div class="col-sm-4 about-line text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-star"></i>
+              
+<h4>WHY WAS TEMPLE BUILD?</h4>
+              
+<p class="black">A Hindu temple, often mandir in India, is a symbolic house, seat and body of divinity for Hindus. It is a structure designed to bring human beings and gods together, using symbolism to express the ideas and beliefs of Hinduism</p>
+            
+</div> <!-- / margin -->
+          
+</div><!-- /col -->
+          
+<div class="col-sm-4 text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-home"></i>
+
+              
+<h4>HOW WAS TEMPLE BUILD?</h4>
+              
+<p class="black">These carved stones, pillars, walls, floors and others are used for building the structure of a stupa or a temple. Apart from this the devotees visiting this temple or stupa bring various offerings which are used for the decoration of the structures.</p>
+            
+</div> <!-- / margin -->
+          
+</div><!-- /col -->
+        
+</div> <!-- /row -->
+         
+         
+         
+         
+     
+</div>
+ </section>
+ 
+ 
+ 
+<!-- [/ABOUTUS]
+ 
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+ 
+ 
+<!-- [INSPIRATION]
+ 
+============================================================================================================================-->
+ 
+<section class="inspiration" id="four">
+     
+<div class="overlay">
+         
+<div class="container">
+             
+<div class="row">
+                
+ <article class="col-md-12 text-center">
+           
+ <div class="intermediate-container">
+             
+ <div class="subheading">
+                 
+ <h4>Are You Ready To <span class="themecolor">Explore</span></h4>
+           
+   </div>
+             
+ <div class="heading">
+              
+  <h2>Hurry up!</h2>
+          
+    </div>
+              
+<div class="">
+              
+  <a class="btn btn-custom-outline" href="#"><span>get started</span></a>
+        
+      </div>
+        
+    </div>
+       
+   </article>
+       
+      </div>
+    
+    </div>
+    
+ </div>
+ 
+</section>
+ 
+ 
+<!-- [/INSPIRATION]
+ 
+
+============================================================================================================================-->
+ 
+ 
+<!-- [SERVICES]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<section class="services white-background black" id="seven">
+     
+ <div class="container">
+        
+<div class="row text-center">
+          
+<div class="col-md-12">
+              
+<h3 class="title">Temple <span class="themecolor">In</span></h3>
+            
+<p class="a-slog">It has essential feature an inner sanctum, the garbha griha or womb-chamber, where the primary Murti or the image of a deity is housed in a simple bare cell.</p>
+          
+</div> <!-- /col -->
+        
+</div> <!-- /row -->
+        
+<div class="gap"></div>
+
+        
+<div class="row">
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+              
+<i class="fa fa-paperclip"></i>
+              
+<div class="nim-service-detail">
+                
+<h4>Tiruavinankudi Temple (Pazhani)</h4>
+                
+<p>Thiru Avinankudi temple is one of the six abodes of Lord Muruga who is considered as the Lord of Kurinji (mountain). The temple got its name from the place it is situated whereas the shrine is called as Kulandai Vēlālyudhaswāmi Temple. It is believed as the earliest abode of Lord Muruga which began under the shade of ‘Nelli’ (Amla) Tree. This temple is about 200 meters from the foothill of main temple. It is one of the temples similar to Chozha architecture. </p>
+   
+           </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+             
+ <i class="fa fa-fire"></i>
+             
+ <div class="nim-service-detail">
+              
+  <h4>Meenakshi amman temple (madurai)</h4>
+              
+  <p>Madurai Meenakshi Sundareswarar temple was built by Pandayan Emperor Sadayavarman Kulasekaran I (1190 CE–1205 CE). He built the main Portions of the three-storeyed gopura at the entrance of Sundareswarar Shrine and the central portion of the Goddess Meenakshi Shrine are some of the earliest surviving parts of the temple.  Maravarman Sundara Pandyan I built a gopura in 1231, then called Avanivendaraman, later rebuilt, expanded and named as Sundara Pandya Thirukkopuram.</p>
+ 
+             </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+              
+<i class="fa fa-bell"></i>
+              
+<div class="nim-service-detail">
+                
+<h4>Saniswaran Temple (Thirunallar)</h4>
+                
+<p>Tirunallar Saniswaran Temple or Dharbaranyeswarar Temple is one of the navagraha temples dedicated to Lord Shani (Saturn) in Karaikal district of Pondicherry, India. The presiding deity is Lord Shiva, Dharbaranyeswar and the place Thirunallar was historically called Dharbaranyam.This is the only temple where Lord Shaneeshwara lost all his power to Lord Shiva (Dharbaranyeshwarar) which saved his devotee Nalan from Saturn’s curses. It is believed that by bathing in Nalan theertham here, one wash off all kinds of misfortunes and afflictions caused by one’s past karmas.
+</p>
+ 
+             </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+         
+ </div> <!-- /col -->       
+        
+</div> <!-- end row -->
+
+        
+<div class="row">
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+             
+ <i class="fa fa-eject"></i>
+             
+ <div class="nim-service-detail">
+               
+ <h4>Brihadishvara Temple (Thanjavur)</h4>
+                
+<p>Brihadishvara Temple (originally known as Peruvudaiyar Kovil) locally known as Thanjai Periya Kovil, and also called Rajarajeswaram, is a Hindu Dravidian styled temple dedicated to the god Shiva located in South bank of Cauvery river in Thanjavur, Tamil Nadu, India.It is one of the largest Hindu temples and an exemplary example of a fully realized Tamil architecture.It is called as Dakshina Meru (Meru of south).Built by Chola emperor Rajaraja I between 1003 and 1010 AD</p>
+          
+    </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+         
+ </div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+           
+ <div class="nim-service margin-bottom">
+              
+<i class="fa fa-leaf"></i>
+              
+<div class="nim-service-detail">
+                
+<h4>Manakula Vinayagar Temple (pondy)</h4>
+                
+<p>This 500-year-old temple is dedicated to Lord Vinayaka or Ganesha.The name Manakula Vinayagar Temple comes from the words Manal meaning sand and Kulam means pond near the sea. The temple has stood tall since before the French Occupation of this area and attracts devotees from far-off places. The temple was earlier laden with sand and thus got the name Manal Kulathu Vinayagar which was later changed to Manakula Vinayagar Temple. </p>
+             
+ </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+             
+ <i class="fa fa-eye"></i>
+              
+<div class="nim-service-detail">
+               
+ <h4>Kailasanathar Temple (Kanchipuram)</h4>
+                
+<p>The temple was built around 700 CE with additions in the 8th- and restorations in later centuries. It is the first structural temple built in South India by Narasimhavarman II (Rajasimha),also known as Rajasimha Pallaveswaram. His son, Mahendravarman III, completed the front façade and the gopuram (tower). Prior temples were either built of wood or hewn into rock faces in
+caves or on boulders, as seen in Mahabalipuram.</p>
+              
+</div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->         
+        
+</div> <!-- end row -->
+
+</div>  <!-- container -->
+    
+</section>
+ 
+ 
+ 
+<!-- [/SERVICES]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+ 
+ 
+ 
+<!-- [FOOTER]
+ 
+============================================================================================================================-->
+ 
+
+<footer class="site-footer section-spacing text-center " id="eight">
+    
+  
+<div class="container">
+    
+<div class="row">
+      
+<div class="col-md-4">
+        
+<p class="footer-links"><a href="#">Terms of Use</a> <a href="#">Privacy Policy</a></p>
+      
+</div>
+      
+<div class="col-md-4"> <small>&copy; 2022, Brought To You By <a href="https://media1.thehungryjpeg.com/thumbs2/ori_3901257_cplzawh5vbs7876vghrvlqk0ju98b3un5qmpgyue_kc-monogram-logo-v5.jpg3">"KC"</a> </small></div>
+      
+<div class="col-md-4"> 
+        
+<!--social-->
+        
+        
+<ul class="social">
+          
+<li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter "></i></a></li>
+          
+<li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+          
+<li><a href="https://www.youtube.com/" target="_blank"><i class="fa fa-youtube-play"></i></a></li>
+        
+</ul>
+        
+        
+<!--social end--> 
+        
+      
+</div>
+    
+</div>
+  
+</div>
+
+</footer>
+
+ 
+ 
+ 
+<!-- [/FOOTER]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+
+</div>
+ 
+
+<!-- [ /WRAPPER ]
+
+=============================================================================================================================-->
+
+	
+<!-- [ DEFAULT SCRIPT ] -->
+	
+<script src="library/modernizr.custom.97074.js"></script>
+	
+<script src="library/jquery-1.11.3.min.js"></script>
+        
+<script src="library/bootstrap/js/bootstrap.js"></script>
+	
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>	
+	
+<!-- [ PLUGIN SCRIPT ] -->
+        
+<script src="library/vegas/vegas.min.js"></script>
+	
+<script src="js/plugins.js"></script>
+        
+<!-- [ TYPING SCRIPT ] -->
+         
+<script src="js/typed.js"></script>
+         
+<!-- [ COUNT SCRIPT ] -->
+         
+<script src="js/fappear.js"></script>
+       
+<script src="js/jquery.countTo.js"></script>
+	
+<!-- [ SLIDER SCRIPT ] -->  
+        
+<script src="js/owl.carousel.js"></script>
+         
+<script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+        
+<script type="text/javascript" src="js/SmoothScroll.js"></script>
+        
+        
+<!-- [ COMMON SCRIPT ] -->
+	<script src="js/common.js"></script>
+  
+</body>
+
+
+</html>
